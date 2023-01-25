10f01901f12375ef17e39aa9d4f5bff5575514b3
pushkar gautam <pushkar.gautam1994@gmail.com>
2023-01-25 14:53:08 +0545

    responsive design bcthankyou page

--- src/Pages/BcThankyou.tsx
+++ src/Pages/BcThankyou.tsx
@@ -7,12 +7,14 @@ const BcThankyou = () => {
 
   const topBackgroundImageStyle = {
     backgroundImage: `url("${TopBackgroundImg}")`,
-    backgroundSize: 'cover'
+    backgroundSize: 'cover',
+    backgroundPosition: 'center'
   }
 
   const footerBackgroundImageStyle = {
     backgroundImage: `url("${FooterBackgroundImg}")`,
     backgroundSize: 'cover',
+    backgroundPosition: 'center'
   }
 
   return (
@@ -23,27 +25,26 @@ const BcThankyou = () => {
           style={topBackgroundImageStyle}
         >
           {/* <div className='bg-gradient-to-r from-black'> */}
-            <div className='pt-16' >
+            <div className='pt-12' >
               <h1 className='text-3xl font-bold'>Thankyou for downloading our Business Central ebook</h1>
-              <h1 className='text-3xl font-bold pl-12'></h1>
             </div>
           {/* </div> */}
         </div>
 
         {/* Middle Banner */}
-        <div className='p-12 grid grid-cols-2 gap-8'>
+        <div className='p-12 md:grid grid-cols-2 gap-8'>
           <div className='flex justify-center'>
             <img src={BCEbook} alt="" className="h-96" /> 
           </div>
           <div className='p-7'>
-            <h3 className='text-2xl py-2 max-w-lg'>We have sent you an email with the PDF version so that you can access it anytime!</h3>
+            <h3 className='text-2xl py-2 max-w-md'>We have sent you an email with the PDF version so that you can access it anytime!</h3>
             <p className='py-8 text-grey max-w-md'>If you have further questions about the products, our experts are happy to help you out.Schedule a call here.</p>
             <button className='bg-primary w-44 h-12 p-3 py-2 text-white rounded'>TALK TO EXPERTS</button>
           </div>
         </div>
 
         {/* Footer */}
-        <div className='grid grid-rows-2'>
+        <div className='md:grid grid-rows-2'>
           <div
             className='flex bg-black text-primary p-16 py-18 h-96'
             style={footerBackgroundImageStyle}
@@ -51,17 +52,17 @@ const BcThankyou = () => {
             <div className='flex flex-col bg-gradient-to-r from-black'>
               <div className='pl-8 h-96'>
                 <h1 className='text-white text-4xl pt-4'>Get in Touch!</h1>
-                <p className='py-8 text-grey max-w-md'>Refer us leads and start earning massive commissions already! Or find honest solutions to all your queries</p>
-                <button className='bg-primary p-3 py-2 w-40 h-12 text-white rounded'>CONTACT US</button>
+                <p className='py-4 text-grey max-w-md'>Refer us leads and start earning massive commissions already! Or find honest solutions to all your queries</p>
+                  <button className='bg-primary p-3 py-2 w-40 h-12 text-white rounded'>CONTACT US</button>
               </div> 
             </div>
           </div>
 
-          <div className='bg-footerbg text-white h-72'>
-            <div className='p-16 grid grid-cols-2'>
-              <div className=''>
+          <div className='bg-footerbg text-white md:h-72'>
+            <div className='p-16 md:grid grid-cols-2'>
+              <div className='sm:pb-3'>
                 <div>DogmaGroup</div>
-                <p className='py-4 text-grey'>A group of companies helping you understand the fuller picture of cutting edge technology and implement it effectively.</p>
+                <p className='py-4 text-grey sm:max-w-sm'>A group of companies helping you understand the fuller picture of cutting edge technology and implement it effectively.</p>
                 <div className='flex flex-row gap-6'>
                   <div>SeeLogic Logo</div>
                   <div>Sirus App Logo</div>
